70e4d1ff5469778247182f6129ceb1bf46653716
Edward England <edwardengland@hotmail.co.uk>
2019-01-26 18:50:07 +0000

    Added Task 1D part 2 to geo

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,20 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+
+# Task 1D (2)
+
+def stations_by_river(stations):
+    """takes list of station objects
+        returns dictionary of format {river: list of stations on this river}"""
+   
+    stations_by_river_dictionary = {}
+    
+    # if station is in dictionary, add to to appropriate list, else create new key and item
+    for object in stations:
+        if object.river in stations_by_river_dictionary:
+            stations_by_river_dictionary[object.river].append(object)
+        else:
+            stations_by_river_dictionary[object.river] = [object]
+    return stations_by_river_dictionary
