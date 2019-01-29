8a86f712d7aa95f3f11dca184d6cec24dc77d349
Shannen Zeller <shannen.zeller@gmail.com>
2019-01-29 19:32:46 +0000

    Created Task1E.py

---
+++ Task1E.py
@@ -0,0 +1,14 @@
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    """Requirements for Task 1E"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
