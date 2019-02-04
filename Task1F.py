8cbec252ea073fa94a150ef622322a4b6e08378d
Edward England <edwardengland@hotmail.co.uk>
2019-02-04 14:35:57 +0000

    started and completed task1f

---
+++ Task1F.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
+
+def run():
+    """Requirements for Task 1F"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    listy = inconsistent_typical_range_stations(stations)
+
+    output = []
+
+    for i in listy:
+            output.append(i.name)
+
+    print( sorted(output))
+
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
