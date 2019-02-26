42d9fde7b3b06867bb9d5b2b52aca04df465b8c6
Edward England <edwardengland@hotmail.co.uk>
2019-02-26 13:28:59 +0000

    finished task2f

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,18 +2,17 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 from floodsystem.stationdata import build_station_list, update_water_levels
-from datetime import datetime, timedelta
+import datetime
 from floodsystem.datafetcher import fetch_measure_levels
 
-stations = build_station_list()
-for i in stations: 
-    dt = 10
-    dates, levels = fetch_measure_levels(i[0].measure_id, dt = datetime.timedelta(days = dt))
-
 def polyfit(dates, levels, p):
+    # function returns tuple of (polynomial, shift of time axis)
+ 
     x = matplotlib.dates.date2num(dates)
     y = levels
 
-    p_coeff = np.polyfit(x, y, p)
-    plt.plot(x, y, '.')
-
+    p_coeff = np.polyfit(x-x[0], y, p)
+    
+    poly = np.poly1d(p_coeff)
+    
+    return (poly, x[0])
